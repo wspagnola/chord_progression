59a7381a57df44d9b8ed87a30dd7784b574592a8
Vergara-Spagnola <vergara-spagnola@Vergara-Spagnolas-MacBook-Pro.local>
2019-02-25 15:25:54 -0500

    created separate script for obtaining chord progression info from  Hook Theory Website

--- music_trends.R
+++ music_trends.R
@@ -92,58 +92,6 @@ sample_song <- hot_100_df %>%
                   dplyr::filter(Year %in% 1958) %>%  
                   dplyr::sample_n(1)
 
-#### Work with HookTheory API ####
-
-
-
-
-api_call <- 'https://api.hooktheory.com/v1/'
-
-#Retrieve ID and activkey
-pars <- list(
-  username = 'wspagnola',
-  password = 'Acadia125!'
-)
-POST(paste0(api_call, 'users/auth'), body = pars) %>%  content
-id <- 187488
-activkey <- "ceb222d144fc6b3b13f9e3dce2b2bd63"
-authorization <- paste("Bearer", activkey)
-
-#Look up songs with 1-2-5 Chord progression 
-GET(paste0(api_call, 'trends/songs?cp=1,2,5'), 
-    add_headers(Authorization = authorization)) %>%  content %>% bind_rows
-
-GET(paste0(api_call, 'trends/songs?cp=1,2,5'), 
-    add_headers(Authorization = authorization)) %>%  content %>% bind_rows
-
-#### Scrape Hook Theory Website ####
-
-
-url <- 'https://www.hooktheory.com/theorytab/view'
-
-artist <- 'bj-thomas'
-song <- 'raindrops-keep-fallin-on-my-head'
-
-# Get Artists 
-chords <- paste(url, artist, song, sep = '/') %>% 
-  read_html() %>%    html_text() %>% 
-  str_detect('tspan')
-  html_nodes(css = '.gotham')
- %>% 
-  str_extract_all(pattern = 'tspan')
-  str_replace_all('\n', '')
-require(stringr)
-  
-url <- 'https://www.hooktheory.com/theorytab/view/the-beatles/hey-jude'
-read_html(url)  %>% 
-  html_text() %>%  str_extract("[^svg]+")
-
-    read_json('https://www.hooktheory.com/theorytab/view/the-beatles/hey-jude') %>%  str_extract_all('chord')
-
-?
-      
-      "/theorytab/view/the-beatles/hey-jude#verse"
-      <a href="/theorytab/view/the-beatles/hey-jude#verse" title="Jump to the verse"><span class="tight-top tight">Verse</span></a>
 
 
 #### Work with Spotify API  ####
