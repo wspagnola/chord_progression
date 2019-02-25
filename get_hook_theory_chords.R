a9a9a7a0812e5c09d22915f7fbb19ea860acfb89
Vergara-Spagnola <vergara-spagnola@Vergara-Spagnolas-MacBook-Pro.local>
2019-02-25 16:35:19 -0500

    Created Function to Download Songs by Chord Progression

--- get_hook_theory_chords.R
+++ get_hook_theory_chords.R
@@ -1,5 +1,7 @@
+require(tidyverse)
+require(httr)
 
-#### Work with HookTheory API ####
+#### Get Access to  HookTheory API ####
 
 api_call <- 'https://api.hooktheory.com/v1/'
 
@@ -8,26 +10,89 @@ pars <- list(
   username = 'wspagnola',
   password = 'Acadia125!'
 )
+
+#Request activkey 
 POST(paste0(api_call, 'users/auth'), body = pars) %>%  content
 id <- 187488
 activkey <- "ceb222d144fc6b3b13f9e3dce2b2bd63"
-authorization <- paste("Bearer", activkey)
+authorization <- paste("Bearer", activkey) #Get authorization code
 
-#Look up songs with 1-2-5 Chord progression 
-GET(paste0(api_call, 'trends/songs?cp=1,2,5'), 
-    add_headers(Authorization = authorization)) %>%  content %>% bind_rows
-
-GET(paste0(api_call, 'trends/songs?cp=1,2,5'), 
-    add_headers(Authorization = authorization)) %>%  content %>% bind_rows
+#EXAMPLE: Look up songs with 1-2-5 Chord progression 
+# GET(paste0(api_call, 'trends/songs?cp=1,2,5'), 
+#     add_headers(Authorization = authorization)) %>%  content %>% bind_rows
 
 
+#### Function to Retrieve all songs for a given chord progression ####
+
+
+retrieve_by_cp <- function(cp, page_num = 100){
+  
+  #Convert Chord Progression into a single string separated by commas
+  cp_string <- cp %>%  as.character %>%  paste(collapse = ',')
+  
+  #Create Empty List
+  song_request_list <-vector('list', page_num)
+  
+  #For Loop to retrieve songs; each page contains 20 songs
+  #Breaks when new tibble in list is empty 
+  #Sleeps 1 second after each request; limit = 10 requests / 10 seconds
+  for (i in 1:page_num ) {
+    url <- paste0('trends/songs?cp=', cp_string, '&page=', i)
+    song_request_list[[i]] <-  GET(paste0(api_call, url), 
+                            add_headers(Authorization = authorization)) %>%  
+                        content %>% 
+                        bind_rows
+    
+    if( song_request_list[[i]] %>%  nrow == 0) {
+      break
+    } else{
+      Sys.sleep(1) 
+    }
+  }
+  
+  #Convert List into large tibble
+  song_df <-song_request_list %>% 
+                bind_rows
+                  
+  return(song_df)
+  
+}
+
+
+#### Retrieve Common Chord Progressions ####
+cp_125 <- retrieve_by_cp(cp = c(1,2,5)) 
+cp_14 <- retrieve_by_cp(cp = c(1,4), page_num = 150) 
+cp_14 %>%  View
+
+cp_14 <- retrieve_by_cp(cp = c(1,4), page_num = 150) 
+cp_14 %>%  View
+
+
+cp_1464 <- retrieve_by_cp(cp = c(1,5, 6, 4), page_num = 10) 
+cp_1464 %>%  dim
 
 
 
-#### Scrape Hook Theory Website ####
+#### Retrieve One Page at a Time  ####
+
+page_num <- 5
+cp <- c(1,5, 6, 4)
+cp_string <- cp %>%  as.character %>%  paste(collapse = ',')
+
+url <- paste0('trends/songs?cp=', cp_string, '&page=', page_num )
+GET(paste0(api_call, url), 
+      add_headers(Authorization = authorization)) %>%  
+  content %>% 
+  bind_rows
+
+
+#### Scrape Hook Theory Website (DOESN'T WORK) ####
 
 
 #NOTE: This doesn't work
+
+#Does anyone know how to scrape SVG?  
+#Or are SVG's meant to prevent scraping?
 
 url <- 'https://www.hooktheory.com/theorytab/view'
 
