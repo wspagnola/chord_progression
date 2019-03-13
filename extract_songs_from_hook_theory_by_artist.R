68d86b2ba62b58482698c238e61a9d1f5a7c5057
Vergara-Spagnola <vergara-spagnola@Vergara-Spagnolas-MacBook-Pro.local>
2019-03-12 22:54:44 -0400

    added every option to extrac_songs script

--- extract_songs_from_hook_theory_by_artist.R
+++ extract_songs_from_hook_theory_by_artist.R
@@ -1,25 +1,36 @@
 
-
-
+artist <- 'tom petty'
+$artist <- 'the beatles'
+#artist <- 'elvis presley'
+#artist <- 'the pixies'
 baseURL <- 'https://www.hooktheory.com/theorytab/results/path/'
 
-artist <- 'the beatles'
 url <- paste0(baseURL, artist)
 url <- url %>% str_replace(pattern = ' ', replacement = '+')
 
+page_vec <- NA
 page_vec <- url %>% 
   read_html %>%
   html_nodes(xpath = '//a[@class="button button-xs button-browse button-primary-open "]') %>% 
   html_text()
-
+page_vec
 if(length(page_vec) == 0) {
   
+     songs <- NA
+     artist_vec <- NA
      songs <-  url %>% 
         read_html %>% 
         html_nodes(xpath = '//p[@class ="song"]') %>% 
         html_text()
+     
+     artist_vec <-  url %>% 
+       read_html %>% 
+       html_nodes(xpath = '//p[@class ="artist"]') %>% 
+       html_text()
+     
+     artist_vec <- artist_vec %>%  str_remove('by ')
   
-} else if(length(page_vec) > 0){
+} else if(length(page_vec) > 1){
   
   num_pages <- page_vec %>%  as.numeric %>%  max
   
@@ -30,9 +41,26 @@ if(length(page_vec) == 0) {
                     read_html %>% 
                     html_nodes(xpath = '//p[@class ="song"]') %>% 
                     html_text()})
+  
+  
+  artist_list <- lapply(url,  function(url){
+    url %>% 
+      read_html %>% 
+      html_nodes(xpath = '//p[@class ="artist"]') %>% 
+      html_text()})
+  
   songs <- unlist(songs)
+
+  artist_list <- artist_list %>% lapply(function(x) str_remove(x, pattern = 'by '))
+  artist_vec <- unlist(artist_list)
+} else if(is.na(page_vec)){
+  artist_vec <- artist
+  songs <- NA
+  
 }
 
 
-data.frame(artist, songs)  
-
+#Filter Out Cover Songs
+## Watch out for Singers with Different Bands
+d <- data.frame(Artist = artist_vec, Songs = songs)  
+d[str_detect(tolower(d$Artist), pattern = artist) ,]
