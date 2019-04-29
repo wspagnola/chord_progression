a40e83b8991848e458e4728684545d484baa8dd3
Vergara-Spagnola <vergara-spagnola@Vergara-Spagnolas-MacBook-Pro.local>
2019-04-28 22:28:01 -0400

    added code to expore mising borrowed chords

--- clean_data_fix.R
+++ clean_data_fix.R
@@ -1,14 +1,39 @@
 #Clean Data with Fixed Beatles DataSte
 source('source.R')
-beatles_clean <- read.csv('data/output/beatles_fix.csv')
-View(beatles_clean)
+beatles <- read.csv('data/output/beatles_fix.csv')
+View(beatles)
 
-beatles_clean$roman <- NA
-for(i in 1:nrow(beatles_clean)){
+beatles$roman <- NA
+for(i in 1:nrow(beatles)){
   print(i)
-  beatles_clean$roman[i] <- convert_to_roman(chords =beatles_clean$chords[i], 
-                                             key = beatles_clean$key[i])
+  beatles$roman[i] <- convert_to_roman(chords =beatles$chords[i], 
+                                             key = beatles$key[i])
   
 }
 
 View(beatles_clean)
+#Note
+## Doesn't work
+#### Drive My Car
+#### The End
+#### We can work it out (Bridge)
+
+#Rescrape Because
+
+####  Check NA chords ####
+
+#Check number of song parts with missing chords
+nrow(beatles[grep('NA', beatles$roman) , ])
+
+
+#Check Roman Numerical Analysis on top of Chords 
+na_chords <- beatles[grep('NA', beatles$roman) , ]
+na_chords_roman <- na_chords %>%  
+  select(song, song_parts, key, roman) %>% 
+  rename(chords = roman) %>% 
+  mutate(roman = 1)
+na_chords_chords <- na_chords %>%  
+  select(song, song_parts, key, chords) %>% 
+  mutate(roman = 0)
+na_df <- rbind(na_chords_roman, na_chords_chords )
+na_df %>%  arrange(song, song_parts, roman) %>% View
